b5a750dd22de6cbf752a7d7588cad46bb17897ac
ajharseikh <ajharseikh2@gmail.com>
2024-07-12 13:31:34 +0530

    swagger related changed

--- src/main/java/in/hero/Application.java
+++ src/main/java/in/hero/Application.java
@@ -6,7 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
+// @OpenAPIDefinition(servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
+
+//@OpenAPIDefinition(
+//	    servers = arrayOf(Server(url = "/"))
+//	)
+// @OpenAPIDefinition(servers = {@Server(url = "/", description = "https://sb-rest-h2-db-app-production.up.railway.app")})
+
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://sb-rest-h2-db-app-production.up.railway.app", description = "Production Server") })
 
 @SpringBootApplication
 public class Application {
