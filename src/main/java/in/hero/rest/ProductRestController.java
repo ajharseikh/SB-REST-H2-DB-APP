c92381e783941aea7d40101d5f22a60d9c34e604
ajharseikh <ajharseikh2@gmail.com>
2024-07-12 10:22:39 +0530

    add @CrossOrigin

--- src/main/java/in/hero/rest/ProductRestController.java
+++ src/main/java/in/hero/rest/ProductRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import in.hero.entity.Product;
 import in.hero.repository.ProductRepository;
 
 @RestController
+@CrossOrigin
 public class ProductRestController {
 	
 	private ProductRepository repo;
